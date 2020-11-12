f1edc8ca45ed403168af307a40509850ed177e3e
D-Katt <58660996+D-Katt@users.noreply.github.com>
2020-11-12 16:55:55 +0300

    Image classification web app

--- Image-classifier/image_classifier.js
+++ Image-classifier/image_classifier.js
@@ -3,9 +3,9 @@ var net;
 
 // Function loads MobileNet model
 async function loadModel() {
-    console.log('Loading mobilenet.');
+    console.log('Loading MobileNet.');
     net = await mobilenet.load();
-    document.getElementById('label').innerHTML = 'Model loaded.';
+    document.getElementById('label').innerHTML = 'Модель загружена.';
     console.log('Successfully loaded model.');
 }
 
@@ -24,23 +24,14 @@ async function getLabel() {
     document.getElementById('label').innerHTML = `${result[0].className}: ${probability}%`;
 }
 
-// Function changes the image on HTTP page
-function newImage(url) {
-    document.getElementById('img').src = url
-}
-
-// Function prompts user to enter new url
-function urlEntry() {
-    console.log('Should show entry window...');
-    let newUrl;
-    newUrl = prompt('Enter an image url:', '');
-    if (newUrl != null) {
-        newImage(newUrl)
+// For uploading image from the file slelected by user
+var uploadImageFile = function(event) {
+    var output = document.getElementById('img');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
     }
-}
-
-// On clicking the button we prompt user to enter new url
-document.getElementById('entry').addEventListener('click', urlEntry)
+  }
 
 // On clicking the button we display image label
 document.getElementById('classify').addEventListener('click', getLabel)
